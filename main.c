acdc9e9a4fa9bdf381b447f668fdb0be1f010c9b
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2022-10-18 19:56:38 -0300

    v2

--- main.c
+++ main.c
@@ -5,133 +5,219 @@
 
 struct ProcessData {
   int Ppid;
-	int Pid;
+  int Pid;
   int ArrivalTime;
   int BurstTime;
+  int Priority;
   int WaitTime;
-  int TurnArountTime;
+  int ResTime;
+  int Finishtime;
   int RemainTime;
-  int CompleteTime;
-} Q1[10], Q2[10], PQ[10]; // Fila de IO (PQ) e fila para os demais processos
+  int StartTime;
+};
 
-void arrivalSort (int pnum) {
-  struct ProcessData temp;
-  for (int i=0; i<pnum; i++) {
-    for (int j=i+1; j<pnum; j++) {
-      if (Q1[i].ArrivalTime > Q1[j].ArrivalTime) {
-        temp = Q1[i];
-        Q1[i] = Q1[j];
-        Q1[j] = temp;
-      }
-    }
+typedef struct ProcessData PD;
+PD CurrentProcess;
+
+void readInput (int count, PD* process) {
+  for(int i=0; i<count; i++) {
+    printf("Digite: <Pid> <ArrivalTime> <BurstTime> <Prioridade> para o processo #%d: ", i + 1);
+    scanf("%d %d %d %d",
+      &process[i].Pid, &process[i].ArrivalTime, &process[i].BurstTime, &process[i].Priority
+    );
+    // Armazena o input
+    process[i].Ppid = i + 1;
+    process[i].RemainTime = process[i].BurstTime;
   }
+  puts("");
 }
 
-void readInput (int pnum) {
-  for(int i=0; i<pnum; i++) {
-    Q1[i].Ppid = i + 1;
-    printf("Digite: <Pid> <ArrivalTime> <BurstTime> para o processo #%d: ", i + 1);
-    scanf("%d%d%d", &Q1[i].Pid, &Q1[i].ArrivalTime, &Q1[i].BurstTime);
-    Q1[i].RemainTime = Q1[i].BurstTime;
-  }
-  printf("\n\n");
-}
-
-void printProcessData (int pnum) {
+void printProcessData (int count, PD* process) {
   puts("Processos criados com sucesso:");
-  for(int i=0; i<pnum; i++){
-    printf("Processo #%d : Pid: %d | ArrivalTime: %d | BurstTime: %d\n",
-      Q1[i].Ppid, Q1[i].Pid, Q1[i].ArrivalTime, Q1[i].BurstTime
+  for(int i=0; i<count; i++){
+    printf("Processo #%d : Pid: %d | ArrivalTime: %d | BurstTime: %d | Priority: %d\n",
+      process[i].Ppid, process[i].Pid, process[i].ArrivalTime, process[i].BurstTime, process[i].Priority
     );
   }
-  printf("\n\n");
+}
+
+int arrivalSort (const PD* a, const PD* b) {
+  return (*a).ArrivalTime - (*b).ArrivalTime;
+}
+
+bool pidSort (const PD* a, const PD* b) {
+  return (*a).Pid > (*b).Pid;
+}
+
+bool ppidSort (const PD* a, const PD* b) {
+  return (*a).Ppid > (*b).Ppid;
 }
 
 
-int main (void) {
-  int numProcesses;
-  int i, j;
-  int Clock = 0, Q_process = 0, PQ_process = 0;
-  int quantum1 = 5, quantum2 = 8;
-  int flag = 0; // controla quando um processo vai para Q2
+/* ==========================================================
+   Queue
+   ========================================================== */
+
+struct queue {
+  int head, tail, size;
+  unsigned capacity;
+  PD Data[10];
+};
+
+typedef struct queue Queue;
+
+struct Queue* initQueue (unsigned capacity) {
+  struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+  queue->capacity = capacity;
+  queue->head = queue->size = 0;
+  queue->tail = capacity - 1;
+  return queue;
+}
+
+int isFull (struct Queue* queue) {
+  return (queue->size == queue->capacity);
+}
+
+int isEmpty (struct Queue* queue) {
+  return (queue->size == 0);
+}
+
+void enqueue (struct Queue* queue, PD process) {
+  if (isFull(queue)) return;
+  queue->tail = (queue->tail + 1) % queue->capacity;
+  queue->Data[queue->tail] = process;
+  queue->size = queue->size + 1;
+}
+
+void dequeue (struct Queue* queue) {
+  if (isEmpty(queue)) return;
+  PD dequeuedProcessD = queue->Data[queue->head];
+  queue->head = (queue->head + 1) % queue->capacity;
+  queue->size = queue->size - 1;
+}
+
+PD getHead (struct Queue* queue) {
+  if (isEmpty(queue)) return;
+  return queue->Data[queue->head];
+}
+
+PD getTail (struct Queue* queue) {
+  if (isEmpty(queue)) return;
+  return queue->Data[queue->tail];
+}
+
+/* ==========================================================
+   Priority Queue
+   ========================================================== */
+
+typedef struct node {
+  PD data;
+  int priority; // valores menores possuem maior prioridade
+  struct node* next;
+} Node;
+
+Node* createNode (PD d, int p) {
+  Node* temp = (Node*)malloc(sizeof(Node));
+  temp->data = d;
+  temp->priority = p;
+  temp->next = NULL;
+  return temp;
+}
+
+PD peek (Node** head) {
+  return (*head)->data;
+}
+
+void pop(Node** head) {
+  Node* temp = *head;
+  (*head) = (*head)->next;
+  free(temp);
+}
+
+void push (Node** head, PD d, int p) {
+  Node* start = (*head);
+
+  // Cria um novo nó
+  Node* temp = createNode(d, p);
+
+  // Caso Especial: A cabeça da lista tem menos prioridade que o novo nó. 
+  // Portanto, inserie um novo nó antes do nó principal e altera a cabeça
+  if ((*head)->priority > p) {
+    // Insere novo nó antes da cabeça
+    temp->next = *head;
+    (*head) = temp;
+  } else {
+    // Percorre a lista e encontra a posição 
+    // para inserir o novo n[ó]
+    while (start->next != NULL && start->next->priority < p) {
+      start = start->next;
+    }
+    temp->next = start->next;
+    start->next = temp;
+  }
+}
+
+int isEmptyPQ(Node** head) {
+  return (*head) == NULL;
+}
+
+int main () {
+  int numProcesses = 0;
 
   // Leitura e avaliacao dos parametros de entrada
   puts("Digite: <numero de processos>");
   scanf("%d", &numProcesses);
   printf("Numero de processos inserido: %d\n\n", numProcesses);
 
+  // Aloca espaco para a quantidade de processos inserida
+  PD *process, *processTemp;
+  process = (PD*) calloc(numProcesses, sizeof(PD));
+  if (process == NULL) {
+    puts("--ERRO: malloc");
+    return 1;
+  }
+
   // Preenche dados dos processos
-  readInput(numProcesses);
-  printProcessData(numProcesses);
+  readInput(numProcesses, process);
+  printProcessData(numProcesses, process);
+  processTemp = process;
 
   // Ordena baseado no arrival time
-  arrivalSort(numProcesses);
-  Clock = Q1[0].ArrivalTime;
+  qsort(process, numProcesses, sizeof(process[0]), arrivalSort);
 
-  printf("Processos na fila Q1 com quantum = 5\n");
-  for (i=0; i<numProcesses; i++) {
-    if (Q1[i].RemainTime <= quantum1) {
-      Clock += Q1[i].RemainTime;
-      Q1[i].RemainTime = 0;
-      Q1[i].WaitTime = Clock - Q1[i].ArrivalTime - Q1[i].BurstTime;
-      Q1[i].TurnArountTime = Clock - Q1[i].ArrivalTime;
-      printf("Processo: %d - Pid: %d - RemainTime: %d - WaitTime: %d - TurnArountTime: %d\n",
-        Q1[i].Ppid, Q1[i].Pid, Q1[i].BurstTime, Q1[i].WaitTime, Q1[i].TurnArountTime);
-    } else {
-      Q2[Q_process].WaitTime = Clock;
-      Clock += quantum1;
-      Q1[i].RemainTime -= quantum1;
-      Q2[Q_process].BurstTime = Q1[i].RemainTime;
-      Q2[Q_process].RemainTime = Q2[Q_process].BurstTime;
-      Q2[Q_process].Ppid = Q1[i].Ppid;
-      Q_process = Q_process + 1;
-      flag = 1;
-    }
-  }
+  // Calcula o tempo total de execucao dos processos criados
+  int totalExecutionTime = 0;
+  totalExecutionTime += process[0].ArrivalTime;
 
-  if (flag == 1) {
-    printf("\nProcessos na fila Q1 com quantum = 8\n");
-  }
-
-  for (i=0; i<Q_process; i++) {
-    if (Q2[i].RemainTime <= quantum2) {
-      Clock += Q2[i].RemainTime;
-      Q2[i].RemainTime = 0;
-      Q2[i].WaitTime = Clock - quantum1 - Q2[i].BurstTime;
-      Q2[i].TurnArountTime = Clock - Q2[i].ArrivalTime;
-      printf("Processo: %d - Pid: %d - RemainTime: %d - WaitTime: %d - TurnArountTime: %d\n",
-        Q2[i].Ppid, Q2[i].Pid, Q2[i].BurstTime, Q2[i].WaitTime, Q2[i].TurnArountTime);
+  for(int i=0; i<numProcesses; i++) {
+    if(totalExecutionTime >= process[i].ArrivalTime) {
+      totalExecutionTime = totalExecutionTime + process[i].BurstTime;
     }
     else {
-      PQ[PQ_process].ArrivalTime = Clock;
-      Clock += quantum2;
-      Q2[i].RemainTime -= quantum2;
-      PQ[PQ_process].BurstTime = Q2[i].RemainTime;
-      PQ[PQ_process].RemainTime = PQ[PQ_process].BurstTime;
-      PQ[PQ_process].Ppid = Q2[i].Ppid;
-      PQ_process = PQ_process + 1;
-      flag = 2;
+      int difference = (process[i].ArrivalTime - totalExecutionTime);
+      totalExecutionTime = totalExecutionTime + difference + process[i].BurstTime;
     }
   }
 
-  if (flag == 2) {
-    printf("\nProcessos na fila PQ para IO\n");
+  int ProcessesTAT[totalExecutionTime]; // turn around time
+  for (int i =0; i<totalExecutionTime; i++) {
+    ProcessesTAT[i] = -1;
   }
+  printf("\nTempo total de execução: %d", totalExecutionTime);
 
-  for (i=0; i<PQ_process; i++) {
-    if (i == 0) {
-      PQ[i].CompleteTime = PQ[i].BurstTime + Clock - quantum1 - quantum2;
-    }
-    else {
-      PQ[i].CompleteTime = PQ[i - 1].CompleteTime + PQ[i].BurstTime;
-    }
-  }
+  Queue* Q1 = initQueue(numProcesses);
+  Queue* Q2 = initQueue(numProcesses);
+  Node* PQ = NULL; //Priority Queue para I/O
+  int cpuState = 0; // 0 para oscioso, 1 para ocupado
+  int quantum1 = 5;
+  int quantum2 = 8;
 
-  for (i=0; i<PQ_process; i++) {
-    PQ[i].TurnArountTime = PQ[i].CompleteTime;
-    PQ[i].WaitTime = PQ[i].TurnArountTime - PQ[i].BurstTime;
-    printf("Processo: %d - Pid: %d - RemainTime: %d - WaitTime: %d - TurnArountTime: %d\n",
-        PQ[i].Ppid, PQ[i].Pid, PQ[i].BurstTime, PQ[i].WaitTime, PQ[i].TurnArountTime);
+  CurrentProcess.Pid = -2;
+  CurrentProcess.Priority = 999999;
+
+  int Clock, Q1_process = 0, Q2_process = 0, PQ_process = 0;
+  for(Clock=0; Clock < totalExecutionTime; Clock++) {
+    
   }
 
   return 0;
